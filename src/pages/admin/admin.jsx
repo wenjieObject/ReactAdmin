070d60ca1d2a706f2701c23d76dd79cba58d4af7
wenjieObject <connyoursis@163.com>
2020-05-07 12:27:30 +0800

    login v0.1

---
+++ src/pages/admin/admin.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+// 后台管理的路由组件
+
+class Admin extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {  }
+    }
+    render() { 
+        return ( 
+            <div>
+                admin
+            </div>
+         );
+    }
+}
+ 
+export default Admin;
---
+++ src/pages/login/login.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+import './css/login.less'
+import { Form, Input, Button } from 'antd';
+import { UserOutlined, LockOutlined } from '@ant-design/icons';
+
+
+// 登录的路由组件
+
+class Login extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+
+
+
+    render() {
+        return (
+
+            <div style={{ height: '100%' }} >
+
+                <div className='loginheader'>react admin</div>
+                <div className='loginbody'>
+                    <div className='loginForm'>
+                        <div className='loginFormHead'>welcome to sign in</div>
+                        <Form
+                            name="normal_login"
+                            className="login-form"
+                            initialValues={{ remember: true }}
+                            onFinish={this.onFinish}
+                        >
+                            <Form.Item
+                                name="username"
+                                rules={[{ required: true, message: 'Please input your Username!' }]}
+                            >
+                                <Input prefix={<UserOutlined className="site-form-item-icon" />} placeholder="Username" />
+                            </Form.Item>
+                            <Form.Item
+                                name="password"
+                                rules={[{ required: true, message: 'Please input your Password!' }]}>
+                                <Input
+                                    prefix={<LockOutlined className="site-form-item-icon" />}
+                                    type="password"
+                                    placeholder="Password"/>
+                            </Form.Item>
+
+
+                            <Form.Item>
+                                <Button type="primary" htmlType="submit" className="login-form-button">
+                                    Log in
+                                </Button>
+                            </Form.Item>
+                        </Form>
+                    </div>
+                </div>
+            </div>
+
+        );
+    }
+
+     onFinish = values => {
+        console.log('Received values of form: ', this);
+        this.props.history.push("/");  
+    };
+
+}
+
+
+
+export default Login;
